cf5e82b9483a354940f9bad3178c387fdc8b2efa
Alec Reimel <areimel@Oxfordcommunications.com>
2020-06-24 17:37:34 -0400

    6-24-20

--- devkit.js
+++ devkit.js
@@ -1,11 +1,25 @@
 /********************************************
 *********************************************
 
-Holy Mountain | Dev Enhancement Suite
+Holy Mountain | Dev Enhancement Kit
 
 *********************************************
 ********************************************/
 
+
+/********************************************
+
+INITITALIZATION
+	- 	Define global vars, including keystrokes, 
+		clicks, and custom console styling.
+
+********************************************/
+var HolyMountainDevKit = {};
+HolyMountainDevKit.console_styling = "background-color: #282828; color: #33FF33; font-size: 16px; padding: 10px 15px;";
+
+$(document).ready(function(){
+	console.log('%c'+ 'Holy Mountain dev enhancement kit initializied.', HolyMountainDevKit.console_styling);
+});
 
 
 
@@ -17,11 +31,15 @@ LINK FORMATTERS
 	
 	/********************************************
 	TAB INDEX
-		- Allows <a> elements without an href to be tab-able
+		- 	Allows <a> elements without an href to be tab-able
 	********************************************/
 	$(document).ready(function(){
+
+		
+
 		$('a[tabindex]').each(function(){
-			console.log('Manual tab-index on page');
+			$(this).addClass('--manual_tab_index');
+			console.log('%c Manual tab-index on page. You should double check it. Search for "--no_href_detected".', MyData.console_styling);
 		});
 
 		$('a:not(a[href])').each(function(){
@@ -37,8 +55,8 @@ LINK FORMATTERS
 
 	/********************************************
 	LINK TARGET="_BLANK"
-		- Scans page for links that lead outside the site and 
-		automatically sets them to open in a new tab.
+		- 	Scans page for links that lead outside the site and 
+			automatically sets them to open in a new tab.
 	********************************************/
 	$(document).ready(function(){
 		$('a[href]').each(function(){
@@ -48,16 +66,16 @@ LINK FORMATTERS
 				
 			} else {
 				$(this).attr('target', '_blank');
-				console.log('absolute link caught');
+				//console.log('absolute link caught');
 			}
 		})
 	});
 
 	/********************************************
 	LINK REL="NOOPENER"
-		- This code sets rel="noopener" for all links with target="_blank"
-		This keeps the new page isolated from the first page, which is more secure
-		and keeps resources freed up.  Works in conjunction with the TARGET code above.
+		- 	This code sets rel="noopener" for all links with target="_blank"
+			This keeps the new page isolated from the first page, which is more secure
+			and keeps resources freed up.  Works in conjunction with the TARGET code above.
 	********************************************/
 	$(document).ready(function(){
 		$('a[target="_blank"]').attr('rel','noopener');
@@ -73,15 +91,15 @@ IMAGE FORMATTERS
 
 	/********************************************
 	ALT TAG FALLBACK
-		- Inserts empty ALT tags on imgs that are missing them altogether.
-		Also console.log's an alert with the 
+		- 	Inserts empty ALT tags on imgs that are missing them altogether.
+			Also console.log's an alert with the 
 	********************************************/
 	$(document).ready(function(){
 		$('img:not(img[alt])').each(function(){
 			var img_src = $(this).attr('src');
 			$(this).attr('alt', '');
 			$(this).addClass('--alt_tag_fallback');
-			console.log('missing alt tag found - src: '+img_src+'. Search for "--alt_tag_fallback".');
+			console.log('%c missing alt tag found - src: '+img_src+'. Search for "--alt_tag_fallback".', MyData.console_styling);
 		});
 		
 	});
@@ -96,7 +114,7 @@ INLINE CSS SCANNER
 	$(document).ready(function(){
 		$('[style]').each(function(){
 			$(this).addClass('--inline_css_detected');
-			console.log('Element with inline CSS found. Search for "--inline_css_detected".');
+			console.log('%c Element with inline CSS found. Search for "--inline_css_detected".', MyData.console_styling);
 
 		});
 		
