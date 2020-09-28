6fc2fe37f72a5180bc69899148063560cb5effda
Alec Reimel <alecreimel1@gmail.com>
2020-09-28 11:33:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
 
-/********************************************
 
-GA AUTO-TAGGER
-
-********************************************/
-	
-	/***** NAV *****/
-		var category	= "Nav";
-		var action		= "Click";
-		var value		= "NULL";
-
-		$('nav a').autotagger(category, action, value);
-
-	/***** FOOTER *****/
-		var category	= "Footer";
-		var action		= "Click";
-		var value		= "NULL";
-
-		$('footer a').autotagger(category, action, value);
-	
-	/***** INITIALIZE *****/
-		$('html').eventfire_init();
 });
